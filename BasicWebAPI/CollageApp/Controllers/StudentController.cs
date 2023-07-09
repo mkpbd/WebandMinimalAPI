16697914d855e2d24416a07adebc92f21050c05b
mkpbd <mostofakamal1990@gmail.com>
2023-07-09 15:04:02 +0600

    Result Type and Request Process Sand

--- BasicWebAPI/CollageApp/Controllers/StudentController.cs
+++ BasicWebAPI/CollageApp/Controllers/StudentController.cs
@@ -19,24 +19,58 @@ namespace CollageApp.Controllers
 
         [HttpGet("GetAllStudents")]
         //[Route("GetAllStudent")]
-        public IEnumerable<Student> GetAllStudents()
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
         {
-            return CollegeRepository.Students;
+            var result = CollegeRepository.Students;
+            if (result == null || result.Count <= 0)
+            {
+                // 204 No Content 
+                return NoContent();
+            }
+            
+            return Ok( CollegeRepository.Students);
         }
 
         [HttpGet("{id:int}", Name = "GetStudentById")]
-        public Student GetStudentById(int id)
+        public ActionResult<Student> GetStudentById(int id)
         {
-            return CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+            if(id <= 0)
+            {
+                // 400 Bad Request client Error 
+                return BadRequest();
+            }
+            if(CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault() == null)
+            {
+                // Not found Error  404  means this data is not  find server side 
+                return NotFound();
+            }
+            // Ok 200 reques is success and Reponse is Ok
+            return Ok(CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault());
         }
 
         [HttpDelete("{id:int}", Name = "StudnetDelete")]
-        public bool StudnetDelete(int id)
+        public ActionResult< bool> StudnetDelete(int id)
         {
+            if(id <= 0 )
+            {
+                // 400 BadRequest from client 
+                return BadRequest();
+            }
             var studnet = CollegeRepository.Students.Where(x => x.Id != id).FirstOrDefault();
 
-            if (studnet != null) return true;
-            else return false;
+            if (studnet == null)
+            {
+                // 404 not found  -> client site erorr
+                return NotFound(false);
+            }
+            if (studnet != null) return Ok(true);
+            else return Ok(false);
+        }
+
+        [HttpGet("GetUsers")]
+        public IActionResult GetUsers()
+        {   
+            return Ok( CollegeRepository.Users);
         }
     }
 }
--- BasicWebAPI/CollageApp/Repository/CollegeRepository.cs
+++ BasicWebAPI/CollageApp/Repository/CollegeRepository.cs
@@ -10,5 +10,26 @@ namespace CollageApp.Repository
                  new Student { Id = 1, Address="abc", Email ="mostoakamal@gmail.com", StudentName="kamal"},
                  new Student { Id = 2, Address="jamal", Email ="jamal@gmail.com", StudentName="jamal"},
              };
+        public static List<UserModel> Users { get; set; } = new List<UserModel>
+            {
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "Ram",
+                    Company = "Mindfire Solutions"
+                },
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "chand",
+                    Company = "Mindfire Solutions"
+                },
+                new UserModel
+                {
+                    UserId = 1,
+                    UserName = "Abc",
+                    Company = "Abc Solutions"
+                }
+        };
     }
 }
