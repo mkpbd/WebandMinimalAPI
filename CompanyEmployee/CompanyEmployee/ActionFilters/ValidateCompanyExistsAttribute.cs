0bdb065224deefd37e50888638866df8e46dbcd5
mostofa kamal <mostofakamal1990@gmail.com>
2023-07-13 05:30:54 +0600

    Filter validations

---
+++ CompanyEmployee/CompanyEmployee/ActionFilters/ValidateCompanyExistsAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployee.ActionFilters
+{
+    public class ValidateCompanyExistsAttribute : IAsyncActionFilter
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILogger<ValidateCompanyExistsAttribute> _logger;
+        public ValidateCompanyExistsAttribute(IRepositoryManager repository, ILogger<ValidateCompanyExistsAttribute> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var trackChanges = context.HttpContext.Request.Method.Equals("PUT");
+            var id = (Guid)context.ActionArguments["id"];
+            var company = _repository.Company.GetCompany(id, trackChanges);
+            if (company == null)
+            {
+                _logger.LogError($"Company with id: {id} doesn't exist in the database.");
+                context.Result = new NotFoundResult();
+            }
+            else
+            {
+                context.HttpContext.Items.Add("company", company);
+                await next();
+            }
+        }
+    }
+}
