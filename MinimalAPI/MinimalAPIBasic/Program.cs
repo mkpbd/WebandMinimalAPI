9f22a599c23ffe83329ccc01adfc011dd9b6eb54
mostofa kamal <mostofakamal1990@gmail.com>
2023-07-14 06:54:49 +0600

    post API  Created

--- MinimalAPI/MinimalAPIBasic/Program.cs
+++ MinimalAPI/MinimalAPIBasic/Program.cs
@@ -1,5 +1,7 @@
 
+using Microsoft.AspNetCore.Mvc;
 using MinimalAPIBasic.Data;
+using MinimalAPIBasic.Models;
 
 namespace MinimalAPIBasic
 {
@@ -25,17 +27,49 @@ namespace MinimalAPIBasic
                 app.UseSwaggerUI();
             }
 
-
+            // Get All Coupon 
             app.MapGet("/api/coupon", () =>
             {
                 return Results.Ok(CouponStore.Coupons);
             });
 
+            // Gel Coupn By Id 
+
+            app.MapGet("/api/coupon/{id:int}", (int id) =>
+            {
+                return Results.Ok(CouponStore.Coupons.Where(x => x.Id == id).FirstOrDefault());
+            }).WithName("GetCoupon").Produces(200).Produces(404);
+
+            // Post Coupon  add coupon in Coupon list 
+
+            app.MapPost("/api/coupon/", ([FromBody] Coupon coupon) =>
+            {
+                if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+                {
+                    return Results.BadRequest("Invalid id or Coupon Name");
+
+                }
+
+
+                var lastCouponId = CouponStore.Coupons.LastOrDefault();
+                coupon.Id = lastCouponId != null ? lastCouponId.Id + 1 : 1;
+                CouponStore.Coupons.Add(coupon);
+
+                //return Results.Ok(coupon);
+
+                //return Results.Created($"api/coupon/{coupon.Id}", coupon);
+
+                return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, coupon);
+
+
+            }).WithName("CreateCoupon").Produces(201).Produces(400);
+
+
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-     
+
 
             app.Run();
         }
